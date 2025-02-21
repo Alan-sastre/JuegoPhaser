b428b453b2c2d75a7603c5488bc29ac8ee7158df
ALAN <alandino2004@gmail.com>
2025-02-21 14:19:16 -0500

    qwe

--- scenas/scenaJuego.js
+++ scenas/scenaJuego.js
@@ -464,7 +464,7 @@ class scenaJuego extends Phaser.Scene {
 
     const offsetX = width * 0.19;
     const offsetY = height * 0.19;
-    const startX = width * 0.20;
+    const startX = width * 2;
     const startY = height * 1;
 
     this.input.addPointer(3); // Permite hasta 3 puntos de toque simultáneos
