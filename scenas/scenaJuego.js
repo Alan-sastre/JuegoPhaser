1d947024609dd60fcb621a49b7ab855ace51a24c
ALAN <alandino2004@gmail.com>
2025-02-21 16:03:53 -0500

    nm

--- scenas/scenaJuego.js
+++ scenas/scenaJuego.js
@@ -467,7 +467,7 @@ class scenaJuego extends Phaser.Scene {
     // - Los botones de movimiento se ubicarán en el lado derecho.
     // - El botón de disparo se ubicará en el lado izquierdo.
     const offsetMovement = width * 0.13;
-    const movementStartX = width * 0.85; // Lado derecho
+    const movementStartX = width * 0.25; // Lado derecho
     const movementStartY = height * 0.9;
 
     const shootX = width * 0.15; // Lado izquierdo
